cc2d51c254c380ca4784de0c30c4b3295a0190e0
DevDemo <31072779+skangman@users.noreply.github.com>
2023-12-10 14:33:16 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // api_path: 'http://localhost:3000',
-    api_path: 'https://pos-api-kh7r.onrender.com/',
+    api_path: 'https://pos-api-kh7r.onrender.com',
     token_name: 'pos_token',
     headers: () => {
         return {
